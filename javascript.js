effa81b29f947dbcf8fced14fc131ee3e98886d0
jdeebs <lftosrs@gmail.com>
2023-04-17 21:09:53 -0700

    Reorganize the div containers for better CSS

---
+++ javascript.js
@@ -0,0 +1,31 @@
+let x = 0;
+let y = 0;
+let operator;
+
+function operate(x, y) {
+  
+}
+
+function add(x, y) {
+  let result = x + y;
+  return result;
+}
+console.log(add(5, 5));
+
+function subtract(x, y) {
+  let result = x - y;
+  return result;
+}
+console.log(subtract(5, 5));
+
+function multiply(x, y) {
+  let result = x * y;
+  return result;
+}
+console.log(multiply(5, 5));
+
+function divide(x, y) {
+  let result = x / y;
+  return result;
+}
+console.log(divide(5, 5));
